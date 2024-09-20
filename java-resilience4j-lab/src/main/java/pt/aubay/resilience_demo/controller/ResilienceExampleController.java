6022c2459fd4fd1668525bb6da997f8d076de440
guilherme.sussel <Guilherme.Sussel@ctw.bmwgroup.com>
2024-09-20 19:51:05 +0100

    java lab

--- java-resilience4j-lab/src/main/java/pt/aubay/resilience_demo/controller/ResilienceExampleController.java
+++ java-resilience4j-lab/src/main/java/pt/aubay/resilience_demo/controller/ResilienceExampleController.java
@@ -3,6 +3,10 @@ package pt.aubay.resilience_demo.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 import pt.aubay.resilience_demo.service.ResilienceExampleService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -19,6 +23,9 @@ public class ResilienceExampleController {
 
 
     @GetMapping
+    @CircuitBreaker(name = "backend")
+    @RateLimiter(name = "backend")
+    @Retry(name = "backend")
     public CompletableFuture<String> listingExampleMethod(@RequestParam(name = "delay") boolean delay) {
         log.info("Simulating list...");
         log.info("Delay {}", delay);
